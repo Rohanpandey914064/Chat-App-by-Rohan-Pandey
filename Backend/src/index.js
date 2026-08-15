916ae07d99f1134248530e9f6d8a1e2a5150cbea
Rohan pandey <rohanrock4019@gmail.com>
2026-08-15 15:13:19 +0530

    add database schema

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,9 +1,27 @@
 //const express = require("express");
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
+import {connectDB} from "./lib/db.js";
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 const PORT = process.env.PORT;
-console.log(process.env.NAME);
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
-app.listen(PORT, ()=> console.log(`Rohan bhaiya ka server running on port number ${PORT} or kya bas`));
+app.use(express.json());
+app.use(cors({
+    origin: FRONTEND_URL,
+    credentials: true
+}));
+app.use(clerkMiddleware())
+
+
+app.get("/health", (req, res) => {
+    res.status(200).json({ message: "Server mast chal raha hai" });
+});
+
+app.listen(PORT, ()=> {
+    connectDB();
+    console.log(`Rohan bhaiya ka server running on port number ${PORT} or kya bas`)
+});
---
+++ Backend/src/lib/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+export async function connectDB() {
+    try {
+        const mongoUri = process.env.MONGO_URI;
+        if (!mongoUri) {
+            throw new Error("MONGO_URI is required leke aao usko please ");
+        }
+
+        const conn = await mongoose.connect(mongoUri);
+        console.log("MongoDB connected",conn.connection.host);
+    } catch (error) {
+        console.error("MongoDB connection error:", error.message);
+        process.exit(1); // Exit the process with failure
+    }
+} 
