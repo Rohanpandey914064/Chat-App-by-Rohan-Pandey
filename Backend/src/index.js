1358409bf81eeaa59879082f7fbef63ddc9ebd8a
Rohan pandey <rohanrock4019@gmail.com>
2026-08-15 16:28:06 +0530

    feat: set up Dockerfile and .dockerignore for multi-stage build

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,12 +2,16 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+
+import fs from "fs";
+import path from "path";
 import {connectDB} from "./lib/db.js";
 import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
+const publicDir = path.join(process.cwd(), "public");
 
 app.use(express.json());
 app.use(cors({
@@ -15,6 +19,13 @@ app.use(cors({
     credentials: true
 }));
 app.use(clerkMiddleware())
+
+if(fs.existsSync(publicDir)) {
+    app.use(express.static(publicDir));
+    app.get("/{*any}", (req, res, next) => {
+        res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+    });
+}   
 
 
 app.get("/health", (req, res) => {
